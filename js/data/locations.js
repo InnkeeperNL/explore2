175b0b61f0d012fc4570f1712531a1a61ce2e9e3
InnkeeperNL <hotfmn@gmail.com>
2025-04-15 06:41:08 +0200

    Update locations.js

--- js/data/locations.js
+++ js/data/locations.js
@@ -4,9 +4,7 @@ var all_available_locations = {
 		image: 'locations/dream_TradingCard-2023-08-22T071732.jpg',
 		local_actions:{},
 		cost:{
-			map: 		5,
-			pitchfork: 	2,
-			water: 		50
+			map: 		20,
 		}
 	},
 	forest:{
@@ -17,8 +15,7 @@ var all_available_locations = {
 		image: 'locations/dream_TradingCard-2023-08-17T070201.jpg',
 		local_actions:{},
 		cost:{
-			map: 	1,
-			lumber: 40
+			map: 	5,
 		}
 	}
 };
